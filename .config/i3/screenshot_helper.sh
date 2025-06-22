49ab98afcb0f325a31c743ed32851df069647e67
NolkMan <4n931mail@gmail.com>
2025-06-22 22:31:41 +0200

    Updated i3 config files
    Better multiscreen support
    Separated configs into files
    Screenshots that are simply built-in

---
+++ .config/i3/screenshot_helper.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+	exit 1
+fi
+
+BASE=/tmp/screenshot.tmp
+LOC=$BASE/location
+NAME=$BASE/name
+
+get_location() {
+	TLOC=$(cat $LOC)
+	TNAME=$(cat $NAME)
+	OO=${TLOC}/${TNAME}_$(date +%s).png
+	if [[ $TLOC == "/dev/null" ]]; then 
+		OO=/dev/null
+	fi
+	echo $OO
+}
+
+if [ $1 == "window" ]; then
+	maim -i $(xdotool getactivewindow) -u | tee $(get_location) | xclip -selection clipboard -t image/png
+fi
+
+if [ $1 == "selection" ]; then 
+	maim -s -u | tee $(get_location) | xclip -selection clipboard -t image/png
+fi
+
+if [ $1 == "setname" ]; then
+	TNAME=$(cat $NAME | dmenu)
+	if [[ $TNAME == "" ]]; then
+		TNAME=screencapture
+	fi
+	echo $TNAME > $NAME
+fi
+
+if [ $1 == "setdir" ]; then
+	TLOC=$(find ~ -path '*/.*' -prune -o -type d -printf '%P\n' | dmenu)
+	if [[ $TLOC == "" ]]; then
+		TLOC=/dev/null
+	fi
+	if [[ ! ${TLOC:0:1} == "/" ]]; then
+		TLOC=~/$TLOC
+	fi
+	echo $TLOC > $LOC
+	mkdir $TLOC
+fi
+
+if [ $1 == "init" ]; then
+	mkdir $BASE
+	echo "/dev/null" > $LOC
+	echo "" > $NAME
+fi
+
+
