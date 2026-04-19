0eee1f488e2377182d41dca3e43173ce2376d25a
NolkMan <4n931mail@gmail.com>
2026-04-19 15:03:25 +0200

    Updated Mako so it works with emails

--- .config/mako/mako-pick.sh
+++ .config/mako/mako-pick.sh
@@ -2,7 +2,19 @@
 
 ID="$1"
 
-APP_NAME=`makoctl list | grep "$ID" -A 1 | grep "App name" | cut -d ' ' -f 5- -`
+echo "$ID"
+
+APP_NAME=`makoctl list | grep "$ID" -A 3 | grep "App name" | cut -d ' ' -f 5- -`
+
+echo "$APP_NAME"
+SUB_NAME=`echo "$APP_NAME" | cut -d ':' -f 1 -`
+echo $SUB_NAME
+if [ "$SUB_NAME" = "IMAP MAIL" ] ; then
+	MSG_ID=`echo "$APP_NAME" | cut -d ':' -f 2 -`
+	python ~/.config/waybar/mail/imap_utils.py delete "$MSG_ID"
+	makoctl dismiss -n "$ID" --no-history
+	exit
+fi
 
 echo "$APP_NAME" > /tmp/tmp
 swayjmp "$APP_NAME"
